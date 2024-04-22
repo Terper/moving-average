a67adea0d93bf40e1022634a814fb388df7889c3
Terper <jann.totterman@gmail.com>
2024-04-22 13:34:48 +0300

    moving average implementering av Max

--- app/src/main/java/fi/arcada/sos_projekt_chart_sma/Statistics.java
+++ app/src/main/java/fi/arcada/sos_projekt_chart_sma/Statistics.java
@@ -1,5 +1,39 @@
 package fi.arcada.sos_projekt_chart_sma;
 
-public class Statistics {
+import java.util.ArrayList;
 
+public class Statistics {
+    public static ArrayList<Double> movingAverage(ArrayList<Double> values, int window){
+        boolean isOn = true;
+        ArrayList<Double> results = new ArrayList<Double>();
+
+        if(values.size() < window){
+            results.add(0.0);
+            return results;
+        }
+
+        int i = 0;
+        double medeltal = 0;
+
+
+        //går igänom varje fönster
+        while(isOn){
+            int j = i;
+            int k = 0;
+
+            //räknar medeltalet i det givna fönstrest
+            while(k < window){
+                medeltal += values.get(j);
+                j++;
+                k++;
+            }
+            i++;
+            results.add(medeltal/window);
+            medeltal = 0;
+            if ((values.size() - window) == i )
+                isOn = false;
+
+        }
+        return results;
+    }
 }
